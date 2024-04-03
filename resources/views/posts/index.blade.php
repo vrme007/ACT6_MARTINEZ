594e1f754b542528d8f84ad394ff15bb8054e0d0
ninojofel <martinezninojofel002@gmail.com>
2024-04-03 18:22:30 +0800

    Add show and index view - Edit readme

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity 6 Martinez</title>
+</head>
+<body>
+    <h1>My Activity 6</h1>
+    <ul>
+        <li><a href="/posts/1">Post 1</a></li>
+        <li><a href="/posts/2">Post 2</a></li>
+    </ul>
+</body>
+</html>
